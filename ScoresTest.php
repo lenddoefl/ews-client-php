09402df9365195a38c8e3d98eb4cca57fd0e3c23
Igor Kovalev <i.kovalev@dunice.net>
2017-08-18 17:57:37 +0300

    Improved ScoresTest.php to use it with new refactored class.
    [*]Edited call of new class instance in ScoresTest.php.

--- ScoresTest.php
+++ ScoresTest.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'ScoresController.php';
 
-$loginRequestScores = new EWSPHPClient\ScoresController();
-$loginRequestScores->setIdentifier('TestKeys/Scores/identifier.txt');
-echo $loginRequestScores->callLogin('https://uat-external.eflglobal.com/api/v1/scores/login.json');
+$loginRequestScores = new EWSPHPClient\ScoresController('https://uat-external.eflglobal.com/api/v1/scores/',
+    'TestKeys/Scores/identifier.txt',
+    'TestKeys/ApplicantJourney/decryption.key',
+    'TestKeys/ApplicantJourney/encryption.key');
+echo $loginRequestScores->callLogin();
