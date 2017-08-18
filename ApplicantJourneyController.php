55ec0e269e0860549eed3c1b749cc55fb0808057
Igor Kovalev <i.kovalev@dunice.net>
2017-08-18 15:39:09 +0300

    Started creating methods dummies.
    [+]Added startSession and finishSession endpoints knocking methods.
    TODO need application to start testing and developing.

--- ApplicantJourneyController.php
+++ ApplicantJourneyController.php
@@ -10,6 +10,50 @@ namespace EWSPHPClient
     class ApplicantJourneyController extends EWSPMain
     {
 
+        protected $applicationHash;
+        protected $publicKey;
+        protected $uid;
+
+        protected $sequence = 0;
+
+        public function callStartSession ($data)
+        {
+            $url = $this->url . '/startSession.json';
+            $post = [
+                "authToken"=>  $this->authToken64,
+                "reqToken"=>   $this->reqToken64,
+                "data"=>$data
+            ];
+            try {
+                $response = $this::sendRequest($url, $post);
+
+                $data = \GuzzleHttp\json_encode($response)->data;
+                $this->applicationHash = $data->applicationHash;
+                $this->publicKey = $data->publicKey;
+                $this->uid = $data->uid;
+
+                return $response;
+            } catch (\Exception $e) {
+                return var_dump($e);
+            }
+        }
+
+        public function callFinishSession ($data)
+        {
+            $url = $this->url . 'inishSession.json';
+
+            array_push($data, ['sequence'=> $this->sequence], ['uid'=> $this->uid]);
+
+            $post = [
+                "authToken"=>  $this->authToken64,
+                "reqToken"=>   $this->reqToken64,
+                "data"=>$data
+            ];
+            $response = $this::sendRequest($url, $post);
+            return $response;
+        }
+
+
     }
 }
 
--- EWSPMain.php
+++ EWSPMain.php
@@ -15,6 +15,11 @@ namespace EWSPHPClient
         protected $decryptionKey;
         protected $encryptionKey;
 
+        protected $url;
+
+        protected $authToken64;
+        protected $reqToken64;
+
         //Methods setIdentifier, setDecryptionKey, setEncryptionKey lets user to set keys and identifier if they
         //weren't set in the controller.
 
@@ -33,15 +38,16 @@ namespace EWSPHPClient
             $this->encryptionKey = file_get_contents($encryptionKey);
         }
 
-        function __construct($identifier = null, $decryptionKey = null, $encryptionKey = null)
+        public function setURL ($url) {
+            $this->url = trim($url, "/");
+        }
+
+        function __construct($url = null, $identifier = null, $decryptionKey = null, $encryptionKey = null)
         {
             $this->setIdentifier($identifier);
             $this->setDecryptionKey($decryptionKey);
             $this->setEncryptionKey($encryptionKey);
-        }
-
-        protected static function getKeys ()
-        {
+            $this->setURL($url);
 
         }
 
@@ -63,18 +69,34 @@ namespace EWSPHPClient
             return $response->getBody();
         }
 
-        /**
-         * @param $url
-         * @return mixed
-         * Method takes API login endpoint url and path to identifier file and returns API json answer.
-         */
 
-        public function callLogin ($url)
+        public function callLogin ()
         {
+            $url = $this->url . '/login.json';
             $post = [ "identifier"=> $this->identifier ];
             $response = $this::sendRequest($url, $post);
 
             return $response;
+        }
+
+        public function encoderDecoder ()
+        {
+            $login = \GuzzleHttp\json_decode($this->callLogin());
+
+            $authToken64 = $login->data->authToken;
+            $authToken = base64_decode($authToken64);
+
+            $reqToken64 = $login->data->reqToken;
+
+            $reqToken = base64_decode($reqToken64);
+            $reqToken = openssl_decrypt($reqToken,'AES-128-CBC' ,$this->decryptionKey, OPENSSL_RAW_DATA, $authToken);
+            $reqToken = openssl_encrypt($reqToken,'AES-128-CBC' ,$this->encryptionKey, OPENSSL_RAW_DATA, $authToken);
+            $reqToken64 = base64_encode($reqToken);
+
+            $this->authToken64 = $authToken64;
+            $this->reqToken64 = $reqToken64;
+
+            return [$authToken64, $reqToken64];
         }
     }
 }
