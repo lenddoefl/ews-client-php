36d9067b8502ccb021bafb613090c08e343458a0
Igor Kovalev <i.kovalev@dunice.net>
2017-09-15 11:59:59 +0300

    Improved composer compatibility.
    [*] Now EWSMain seeks autoloader in several paths.

--- EWSMain.php
+++ EWSMain.php
@@ -1,7 +1,18 @@
 <?php
 
 namespace {
-    require 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    $paths = [
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php',
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+        __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+        'vendor', ".."];
+    foreach ($paths as $path) {
+        if (file_exists($path)) {
+            require $path;
+            break;
+        }
+    }
+
 }
 
 
